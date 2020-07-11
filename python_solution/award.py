fd87cc14f786b043df1ca7be4586ae5a37cbd44f
Celestino <celestino.salim@gmail.com>
2020-07-11 16:50:40 -0400

    Added python solution.  Created virtual env and added to git ignore,  removed logic from update_quality  and move it inside the Award class as methods.

--- python_solution/award.py
+++ python_solution/award.py
@@ -3,3 +3,38 @@ class Award(object):
         self.name = name
         self.expires_in = expires_in
         self.quality = quality
+
+    def _valid_quality(self):
+        return self.quality in range(1, 50)
+
+    def _increase_quality_by(self, value):
+        self.quality += value
+
+    def _decrease_quality_by(self, value):
+        self.quality -= value
+
+    def quality_before_expiration(self):
+        if self._valid_quality():
+            if self.name == "Blue First":
+                self._increase_quality_by(1)
+            elif self.name == "Blue Compare":
+                if self.expires_in <= 10:
+                    self._increase_quality_by(3 if self.expires_in <= 5 else 2)
+                else:
+                    self._increase_quality_by(1)
+            elif self.name == "Blue Star":
+                self._decrease_quality_by(2)
+            else:
+                self._decrease_quality_by(1)
+
+    def quality_pass_expiration(self):
+        self.expires_in -= 1
+        if self.expires_in < 0 and self._valid_quality():
+            if self.name == "Blue First":
+                self._increase_quality_by(1)
+            elif self.name == "Blue Compare":
+                self._decrease_quality_by(self.quality)
+            elif self.name == "Blue Star":
+                self._decrease_quality_by(2)
+            else:
+                self._decrease_quality_by(1)
