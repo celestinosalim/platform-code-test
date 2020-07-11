b3615049f6b4ca5287b48c1c57fbce2a7f186497
Celestino <celestino.salim@gmail.com>
2020-07-11 17:11:46 -0400

    Updated logic. Using a dictionary / hash_map as the single source of truth.  making the keys the type/name of the award and the values the amount that is needed to be added/subtracted from the quality.

--- ruby_solution/update_quality.rb
+++ ruby_solution/update_quality.rb
@@ -1,9 +1,5 @@
 require "award"
 
 def update_quality(awards)
-  awards.each do |award|
-    next if award.name == "Blue Distinction Plus"
-    award.handle_before_expiration
-    award.handle_pass_expiration
-  end
+  awards.map(&:calculate_quality)
 end
